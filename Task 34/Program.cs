a23b9e2baae06eb20c3fe16ea86ac6b29a884bdc
elena.bondarchuk <e-bondarchuk@mail.ru>
2022-09-15 14:26:55 +0300

    upd 34

---
+++ Task 34/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите количество элементов в массиве:");
+
+            Random rnd = new Random();
+            int a = 0;
+
+            bool numberCheck = int.TryParse(Console.ReadLine(),out int a_size);
+            if (numberCheck)
+            {
+                int[] a_array = new int[a_size];
+                for (int i = 0; i < a_size; i++)
+                {
+                    a_array[i] = rnd.Next(100, 999);
+                    if (a_array[i] % 2 == 0) a++;
+                }
+                Console.WriteLine("[" + String.Join(", ", a_array) + "] -> " + a);
+
+            }
+            else Console.WriteLine("не число");
